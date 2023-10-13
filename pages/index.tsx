c72c3baec4cb5732b0e40ca3f8e2d2781d1e8b25
alexa <alexamacias192@gmail.com>
2023-10-12 23:47:20 -0400

    Updates file organization, adds basic map component to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import TheMap from '../components/TheMap'
 
 type ConnectionStatus = {
   isConnected: boolean
@@ -11,14 +12,6 @@ export const getServerSideProps: GetServerSideProps<
 > = async () => {
   try {
     await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
 
     return {
       props: { isConnected: true },
@@ -31,6 +24,7 @@ export const getServerSideProps: GetServerSideProps<
   }
 }
 
+
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
@@ -42,9 +36,6 @@ export default function Home({
       </Head>
 
       <main>
-        <h1 className="title">
-          Here we go.
-        </h1>
 
         {isConnected ? (
           <h2 className="subtitle">Connected to Mongo</h2>
@@ -55,29 +46,7 @@ export default function Home({
           </h2>
         )}
 
-
-        <div className="grid">
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <TheMap />
       </main>
 
       <footer>
